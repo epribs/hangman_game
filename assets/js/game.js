6bcfa50a3ad6d248f2fe4c3d8bb24f085ee7165f
epribs <ericpribramsky@gmail.com>
2017-02-11 08:40:11 -0600

    Adding files

---
+++ assets/js/game.js
@@ -0,0 +1,69 @@
+window.onload = function () {
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lives;
+var words = ["biscuit", "bender", "hockey", "puck", "stick", "goal"];
+var wordHolder;
+var guess;
+var guesses = [];
+var score = 0;
+
+var keyGuess = document.onkeyup =
+	function check() {
+		for (var i = 0; i < words.length; i++) {
+			if (words[i] === guess) {
+				guesses[i].innerHTML = guess;
+				counter += 1;
+			}
+		}
+		var g = (words.indexOf(guess));
+		if (g === -1) {
+			lives -= 1;
+			showLives();
+		} else {
+			showLives();
+		}
+	}
+
+showLives = function (){
+	score = 0;
+	document.getElementById("lives").innerHTML = "You have " + lives + " lives";
+	document.getElementById("score").innerHTML = score;
+	if (lives < 1) {
+	  document.getElementById("lives").innerHTML = "Game Over";
+	}
+
+	for (var i = 0; i < guesses.length; i++) {
+	  if (counter === guesses.length) {
+	  	var score =+1;
+	    document.getElementById("lives").innerHTML = "You Win!";
+	    document.getElementById("score").innerHTML = score;
+	  }
+	}
+}
+
+
+result = function() {
+	wordHolder = document.getElementById("wordHolder");
+	correct = document.createElement("ul");
+
+	for (var i = 0; i < words.length; i++) {
+		correct.setAttribute("id", "myWord");
+		guess = document.createElement("li");
+		guess.setAttribute("class", "guess");
+		guess.innerHTML = "_"};
+}
+
+$(".playBtn").on("click", function(){
+	words = words[Math.floor(Math.random() * (words.length - 1))];
+	// chosenWord = words[chooseWord];
+	console.log(words);
+	guesses = [];
+	lives = 9;
+	showLives();
+});
+}
+
+
+
+
