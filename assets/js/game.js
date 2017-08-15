e8212c145c49a19f5d7425a1711aa19e3cc13e0b
epribs <ericpribramsky@gmail.com>
2017-08-15 16:41:09 -0500

    adding most current logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,29 +2,49 @@
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lives;
 var words = ["biscuit", "bender", "hockey", "puck", "stick", "goal"];
+var word;
 // var wordBank = words.split("");
-var wordHolder;
+var wordHold;
 var guess;
 var guesses = [];
 var score = 0;
+var counter = 0;
 
-var keyGuess = document.onkeyup = check();
-	function check() {
-		for (i = 0; i < words.length; i++) {
-			if (words.charAt[i] === guess) {
-				// guesses[i] = guess;
-				// console.log(guesses);
-				// counter += 1;
-			}
-		}
-		var g = (words.indexOf(guess));
-		if (g === -1) {
-			lives -= 1;
-			showLives();
-		} else {
-			showLives();
+$(".playBtn").on("click", function(){
+	word = words[Math.floor(Math.random() * words.length)];
+	// chosenWord = words[chooseWord];
+	console.log(word);
+	guesses = [];
+	lives = 9;
+	showLives();
+	result();
+});
+
+document.onkeyup = function(event) {
+	var guess = String.fromCharCode(event.keyCode).toLowerCase();
+	check(guess);
+};
+
+function
+
+function check(guess) {
+	for (i = 0; i < word.length; i++) {
+		if (word.charAt(i) === guess) {
+			guesses.push(guess);
+			counter += 1;
+			$(".guess").text(guess);
 		}
 	}
+
+	var g = (word.indexOf(guess));
+	if (g == -1) {
+		lives -= 1;
+		showLives();
+	} else {
+		//wordReveal(guess);
+		showLives();
+	}
+}
 
 
 var result = function() {
@@ -55,18 +75,3 @@ var showLives = function (){
 	  }
 	}
 }
-
-
-$(".playBtn").on("click", function(){
-	words = words[Math.floor(Math.random() * words.length)];
-	// chosenWord = words[chooseWord];
-	console.log(words);
-	guesses = [];
-	lives = 9;
-	showLives();
-	result();
-
-});
-
-
-
